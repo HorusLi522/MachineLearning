c1e3441e3fb5700e384465c1e8bd1050667ce669
Ethan Lyon <ethanlyon@gmail.com>
2017-06-23 08:06:16 -0400

    added setup tools

---
+++ machine_learning/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='machine_learning',
+      version='0.0.1',
+      description='Utils for machine learning',
+      author='Ethan Lyon',
+      author_email='ethanyon@gmail.com',
+      url='https://github.com/iethan/machine_learning',
+      packages=['scrapy'],
+     )
