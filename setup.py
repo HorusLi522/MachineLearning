c588fad3f477b4f487c42bbaa33cb57ad82a5999
Ethan Lyon <ethanlyon@gmail.com>
2017-06-23 10:54:14 -0400

    added camel case again

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
-setup(name='machine-learning',
+setup(name='MachineLearning',
       version='0.0.1',
       description='Utils for machine learning',
       author='Ethan Lyon',
       author_email='ethanyon@gmail.com',
       url='https://github.com/iethan/machine_learning',
-      packages=[str('machine_learning')],
+      packages=[str('MachineLearning')],
      )
